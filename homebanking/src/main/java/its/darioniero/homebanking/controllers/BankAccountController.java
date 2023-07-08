225e571554be5bcad3769ad1b549369398d6ebc4
Dario Niero <dario.niero@enginius.com>
2023-07-08 23:36:18 +0200

    add checks in getDepositView controller

--- homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
+++ homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
@@ -1,10 +1,18 @@
 package its.darioniero.homebanking.controllers;
 
+import its.darioniero.homebanking.entities.BankAccount;
+import its.darioniero.homebanking.services.BankAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BankAccountController {
+
+    @Autowired
+    BankAccountService bankAccountService;
 
     @GetMapping
     public String getIndexView(){
@@ -12,7 +20,17 @@ public class BankAccountController {
     }
 
     @GetMapping("/deposit")
-    public String getDepositView(){
+    public String getDepositView(Model model, @RequestParam("bankAccountCode") String code){
+
+
+        BankAccount bankAccount = bankAccountService.fetchBankAccountByCode(code);
+
+        if (bankAccount == null){
+            return "error";
+        }
+
+        model.addAttribute(bankAccount);
+
         return "deposit";
     }
 
