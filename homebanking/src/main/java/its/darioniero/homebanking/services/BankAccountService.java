9ec2b054cc7d1606c535d78a63055ae73b1d8c22
Dario Niero <dario.niero@enginius.com>
2023-07-08 21:46:29 +0200

    create method updateBankAccountBalance

--- homebanking/src/main/java/its/darioniero/homebanking/services/BankAccountService.java
+++ homebanking/src/main/java/its/darioniero/homebanking/services/BankAccountService.java
@@ -1,5 +1,6 @@
 package its.darioniero.homebanking.services;
 
+import its.darioniero.homebanking.entities.BankAccount;
 import its.darioniero.homebanking.repositories.BankAccountRepository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -7,6 +8,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,4 +21,15 @@ public class BankAccountService {
 
     @Autowired
     BankAccountRepository bankAccountRepository;
+
+    public BankAccount updateBankAccountBalance(Long bankAccountCode, BigDecimal deposit) {
+        Optional<BankAccount> bankAccountToUpdate = bankAccountRepository.findById(bankAccountCode);
+        if (bankAccountToUpdate.isPresent()) {
+            BigDecimal newBalance = bankAccountToUpdate.get().getBalance().add(deposit);
+            bankAccountToUpdate.get().setBalance(newBalance);
+            return bankAccountRepository.save(bankAccountToUpdate.get());
+        }
+        return null;
+
+    }
 }
