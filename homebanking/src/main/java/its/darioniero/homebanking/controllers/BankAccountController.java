19a2bfdc2a1089c9d2f1f47aace72c561bf1f6cc
Dario Niero <dario.niero@enginius.com>
2023-07-08 21:11:55 +0200

    add controller for index view

--- homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
+++ homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
@@ -1,4 +1,13 @@
 package its.darioniero.homebanking.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BankAccountController {
+
+    @GetMapping
+    public String getIndex(){
+        return "index";
+    }
 }
