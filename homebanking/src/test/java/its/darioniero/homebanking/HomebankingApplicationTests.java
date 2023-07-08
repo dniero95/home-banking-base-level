2fcae7a34c2332f51c5755bcbd42d02883be167b
Dario Niero <dario.niero@enginius.com>
2023-07-08 20:39:31 +0200

    init spring project

---
+++ homebanking/src/test/java/its/darioniero/homebanking/HomebankingApplicationTests.java
@@ -0,0 +1,13 @@
+package its.darioniero.homebanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
