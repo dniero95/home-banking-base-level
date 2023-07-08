5ade28ca1c53059b64544c3e7bab9dd262f320fb
Dario Niero <dario.niero@enginius.com>
2023-07-08 23:34:48 +0200

    add checks to fetchBankAccountByCode

--- homebanking/src/main/java/its/darioniero/homebanking/services/BankAccountService.java
+++ homebanking/src/main/java/its/darioniero/homebanking/services/BankAccountService.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +35,12 @@ public class BankAccountService {
     }
 
     public BankAccount fetchBankAccountByCode(String code) {
+
+        if (!NumberUtils.isParsable(code)){
+            return null;
+        }
+
+
         Optional<BankAccount> bankAccount = bankAccountRepository.findById(Long.parseLong(code));
         if (bankAccount.isPresent()) {
             return bankAccount.get();
