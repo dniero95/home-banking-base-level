d0c64c633f9e0a1883707a737e0f2179d2208936
Dario Niero <dario.niero@enginius.com>
2023-07-14 16:00:28 +0200

    end

--- homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
+++ homebanking/src/main/java/its/darioniero/homebanking/controllers/BankAccountController.java
@@ -2,11 +2,15 @@ package its.darioniero.homebanking.controllers;
 
 import its.darioniero.homebanking.entities.BankAccount;
 import its.darioniero.homebanking.services.BankAccountService;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 
 @Controller
 public class BankAccountController {
@@ -15,17 +19,17 @@ public class BankAccountController {
     BankAccountService bankAccountService;
 
     @GetMapping
-    public String getIndexView(){
+    public String getIndexView() {
         return "index";
     }
 
     @GetMapping("/deposit")
-    public String getDepositView(Model model, @RequestParam("bankAccountCode") String code){
+    public String getDepositView(Model model, @RequestParam("bankAccountCode") String code) {
 
 
         BankAccount bankAccount = bankAccountService.fetchBankAccountByCode(code);
 
-        if (bankAccount == null){
+        if (bankAccount == null) {
             return "error";
         }
 
@@ -35,12 +39,14 @@ public class BankAccountController {
     }
 
     @GetMapping("/confirm")
-    public String getConfirmView(){
+    public String getConfirmView(Model model, @RequestParam("code") String code, @RequestParam("amount") String amount) {
+        BankAccount updatedBankAccount = bankAccountService.updateBankAccountBalance(Long.parseLong(code), new BigDecimal(amount));
+        model.addAttribute("account", updatedBankAccount);
         return "confirm";
     }
 
     @GetMapping("/error")
-    public String getErrorView(){
+    public String getErrorView() {
         return "error";
     }
 }
